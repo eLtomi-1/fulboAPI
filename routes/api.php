9f822e56fa3fc4b411fb02a76e8c7946c1558c99
eLtomi-1 <tomigonzalezcabrera@gmail.com>
2024-02-15 17:58:54 -0300

    se anadio ruta para listar un solo partido

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::prefix('v1')->group(function () {
     Route::post("/logout",[UserController::class,"cerrarSesionUsuario"])->middleware('auth:api')->middleware(IsUserToken::class);
     Route::post("/match",[UserController::class,"crearPartido"])->middleware('auth:api')->middleware(IsUserToken::class);
     Route::get("/match",[UserController::class,"listarPartidosCreados"])->middleware('auth:api')->middleware(IsUserToken::class);
+    Route::get("/match/{d}",[UserController::class,"listarPartidoCreado"])->middleware('auth:api')->middleware(IsUserToken::class);
     Route::post("/user",[UserController::class,"registrarUsuario"]);
     Route::prefix('oauth')->group(function () {
         Route::get("/user/data",[UserController::class,"validarTokenUsuario"])->middleware('auth:api');
